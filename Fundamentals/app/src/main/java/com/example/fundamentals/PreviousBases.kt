a971e257cd41779ce38d321329f00956988d38da
Pedro Ríos Fernández <peter-puessi@hotmail.com>
2024-05-04 12:26:43 +0200

    Previous Bases - Coroutines Init

--- Fundamentals/app/src/main/java/com/example/fundamentals/PreviousBases.kt
+++ Fundamentals/app/src/main/java/com/example/fundamentals/PreviousBases.kt
@@ -1,5 +1,8 @@
 package com.example.fundamentals
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 import kotlin.random.Random
 
@@ -7,9 +10,39 @@ fun main() {
     lambda()
 
     threads()
+
+    coroutinesVsThreads()
+}
+
+fun coroutinesVsThreads() {
+    newTopic("Coroutines vs Threads")
+
+    // Efficient way of handling 1 million tasks
+    runBlocking {
+        (1..1_000_000).forEach {
+            launch {
+                delay(someTime())
+                print("*")
+            }
+        }
+    }
+
+    /* Inefficient way of handling 1 million tasks
+    (1..1_000_000).forEach {
+        thread{
+            Thread.sleep(someTime())
+            print("*")
+        }
+    }*/
+}
+
+private const val SEPARATOR = "===================="
+fun newTopic(topic: String) {
+    println("\n$SEPARATOR $topic $SEPARATOR\n")
 }
 
 fun threads() {
+    newTopic("Threads")
     println("Thread: ${multiThread(4, 5)}")
     multiThreadLambda(5, 6) {
         println("Thread + Lambda: $it")
@@ -45,6 +78,7 @@ fun multiThreadLambda(x: Int, y: Int, callback: (result: Int) -> Unit) {
 fun someTime(): Long = Random.nextLong(500, 2_000)
 
 fun lambda() {
+    newTopic("Lambda")
     println(multi(2, 3))
 
     println(multiLambda(3, 4) { result ->
